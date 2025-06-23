8619b7d2dcdc19e8768e501c1381d1597302d24b
DrinoSan <s.becirbegovic@gmail.com>
2025-06-23 22:24:06 +0200

    Created makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
-.PHONY: all
+.PHONY: all clean
 
-all:
-	 ./build.sh
+CXX = clang++
+CXXFLAGS = -g `pkg-config --cflags raylib` -Wall -std=c++17
+LDFLAGS = `pkg-config --libs raylib` -lpthread -L/opt/homebrew/lib -lglfw -framework CoreFoundation -framework Cocoa -framework IOKit -framework OpenGL
+
+CPP_FILES := $(wildcard *.cpp)
+OBJ_FILES := $(CPP_FILES:.cpp=.o)
+
+all: wizard
+
+wizard: $(OBJ_FILES)
+	$(CXX) $(OBJ_FILES) $(LDFLAGS) -o $@
+
+%.o : %.cpp
+	$(CXX) -c $(CXXFLAGS) $< -o $@
+
+clean:
+	rm -f *.o wizard
--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main( void )
    //Texture2D tileMapLayout = LoadTexture( "resources/dungonmapAdjusted.tmx" );
    std::ifstream file("resources/dungonmapAdjusted.tmx");
    std::string str;
-   std::vector<int32_t> tileMapLayout{ 0 };
+   std::vector<int32_t> tileMapLayout{ };
    while (std::getline(file, str))
    {
       char* head = str.data();
@@ -40,9 +40,10 @@ int main( void )
       }
    }
 
+
    Texture2D tileMapPNG    = LoadTexture( "resources/kenney_tinyDungeon/Tilemap/tilemap.png" );
    Rectangle tileSrc = { 0.0f, 0.0f, static_cast<float>( tileMapPNG.width / 30 ), static_cast<float>( tileMapPNG.height / 20 ) };
-   Rectangle tileDest= { 0.0f, 0.0f, static_cast<float>( tileMapPNG.width / 30 ), static_cast<float>( tileMapPNG.height / 20 ) };
+   Rectangle tileDest= { 0.0f, 0.0f, 300, 300 };
 
    int framesCounter = 0;
    Player_t player;
@@ -74,7 +75,10 @@ int main( void )
       }
 
       if ( IsKeyDown( KEY_Q ) )
+      {
          break;
+      }
+
       //----------------------------------------------------------------------------------
 
       // Draw
@@ -89,6 +93,41 @@ int main( void )
          //Texture2D tileMapPNG -> contains the whole png texture
          //Rectangle tileSrc = is the recatngle inside the pngTexture
          //Rectangle tileDest= is the rectangle inside our wizard map
+
+         //tileMapPNG 12x12
+         //IndexInSrc = 26
+         auto indexInSrc = tileMapLayout[ i ];
+         auto x = indexInSrc % 12;
+         auto y = indexInSrc % 12;
+         tileSrc.x = x;
+         tileSrc.y = y;
+
+         tileDest.x = i % 30;
+         tileDest.y = i / 20;
+
+         std::cout << "------------------------------\n";
+         std::cout << "indexInSrc: " << indexInSrc << " i: " << i << "\n";
+         std::cout << "tileSrc.x: " << tileSrc.x << " tileSrc.y: " << tileSrc.y << "\n";
+         std::cout << "tileDest.x: " << tileDest.x << " tileDest.y: " << tileDest.y << "\n";
+         std::cout << "++++++++++++++++++++++++++++++\n";
+
+//------------------------------
+//indexInSrc: 1 i: 0
+//tileSrc.x: 1 tileSrc.y: 1
+//tileDest.x: 0 tileDest.y: 0
+//++++++++++++++++++++++++++++++
+//------------------------------
+//indexInSrc: 1 i: 1
+//tileSrc.x: 1 tileSrc.y: 1
+//tileDest.x: 1 tileDest.y: 1
+//++++++++++++++++++++++++++++++
+//------------------------------
+//indexInSrc: 1 i: 2
+//tileSrc.x: 1 tileSrc.y: 1
+//tileDest.x: 2 tileDest.y: 2
+//++++++++++++++++++++++++++++++
+
+         DrawTexturePro( tileMapPNG, tileSrc, tileDest, {0,0}, 0, WHITE );
 
       }
 

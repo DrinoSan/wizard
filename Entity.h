777bb2a5c9c6b77d9551e96cf96c3144263cdf81
DrinoSan <s.becirbegovic@gmail.com>
2025-11-05 22:35:50 +0100

    Some Comments

--- Entity.h
+++ Entity.h
@@ -20,13 +20,16 @@ typedef struct
    Vector2          pos;
    Vector2          velocity;
    Texture2D        texture;
+   // Definingt the rectangle inside the texture
    Rectangle        frame_rec;
+   // Used for Collision stuff
    Rectangle        hitbox;
    int32_t          attack_range;
    int32_t          attack_speed;
    int32_t          attack_power;
 
    // Animation stuff
+   /// Defining y value inside texture/sprite
    float animation_walk_up_y;
    float animation_walk_left_y;
    float animation_walk_down_y;
--- Player.c
+++ Player.c
@@ -1,24 +1,24 @@
-#include "stdlib.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 #include "Player.h"
 
-
 //-----------------------------------------------------------------------------
 Player_t* player_create( ENTITY_TYPE type, float x, float y,
-                           const char* sprite_path )
+                         const char* sprite_path )
 {
    Player_t* player = malloc( sizeof( Player_t ) );
-   if( player == NULL )
+   if ( player == NULL )
    {
       printf( "Could not allocate space for Player\n" );
       return NULL;
    }
 
-   Vector2 pos = {x,y};
-   //Entity_t* entity = entity_create( ENTITY_TYPE_PLAYER, pos.x, pos.y, sprite_path );
+   Vector2 pos = { x, y };
+   // Entity_t* entity = entity_create( ENTITY_TYPE_PLAYER, pos.x, pos.y,
+   // sprite_path );
    Entity_t* entity = entity_create( type, pos.x, pos.y, sprite_path );
-   if( entity == NULL )
+   if ( entity == NULL )
    {
       free( player );
       return NULL;
@@ -34,7 +34,7 @@ Player_t* player_create( ENTITY_TYPE type, float x, float y,
 void player_free( Entity_t* entity );
 
 //-----------------------------------------------------------------------------
-void player_go_Right( Player_t* player );
-void player_go_Left( Player_t* player );
-void player_go_Up( Player_t* player );
-void player_go_Down( Player_t* player );
+void player_go_Right( Player_t* player ) {}
+void player_go_Left( Player_t* player ) {}
+void player_go_Up( Player_t* player ) {}
+void player_go_Down( Player_t* player ) {}
